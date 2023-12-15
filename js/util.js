5bd62f6d496adf6a56e35f17139815b4fee9da70
Nordwi <125876665+Nordwi@users.noreply.github.com>
2023-12-15 15:22:32 +0500

    Add files via upload

---
+++ js/util.js
@@ -0,0 +1,29 @@
+import {MESSAGE} from '/js/data.js';
+import {NAME} from '/js/data.js';
+import {DESCRIPTION} from '/js/data.js';
+
+const createComment = function(id){
+  return {
+    id: id,
+    avatar: `img/avatar-${Math.floor(Math.random() * 5)+1}.svg`,
+    message: MESSAGE[Math.floor(Math.random() * MESSAGE.length)],
+    name: NAME[Math.floor(Math.random() * NAME.length)]
+  };
+};
+
+const createPhoto = function(id){
+  const comments = [];
+  for (let i = 0; i < Math.floor(Math.random() * 30); i++){
+    comments[i] = createComment(Math.floor(Math.random() * 9999999999));
+  }
+  return {
+    id: id,
+    url: `photos/${id+1}.jpg`,
+    description: DESCRIPTION[Math.floor(Math.random() * DESCRIPTION.length)],
+    likes: Math.floor(Math.random() * 176) + 15,
+    comments: comments
+  };
+};
+
+export {createPhoto};
+export{createComment};
