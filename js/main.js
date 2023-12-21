0b9967b8ceaa2ef861cfbfd21733b59934686940
Nordwi <amosh.work@yandex.ru>
2023-12-21 18:39:25 +0500

    server load

---
+++ js/main.js
@@ -0,0 +1,17 @@
+<<<<<<< Updated upstream
+=======
+/* eslint-disable no-console */
+import '/js/forms.js';
+import {doFetch} from './fetch.js';
+import {createError} from './util.js';
+import {createArray} from './util.js';
+
+doFetch(
+  createArray,
+  createError,
+  'get');
+
+
+
+
+>>>>>>> Stashed changes
---
+++ js/util.js
@@ -0,0 +1,55 @@
+import {MESSAGE} from '/js/data.js';
+import {NAME} from '/js/data.js';
+import {DESCRIPTION} from '/js/data.js';
+import {drawPictures} from '/js/draw.js';
+
+const createComment = function(id){
+  return {
+    id: id,
+    avatar: `img/avatar-${Math.floor(Math.random() * 5)+1}.svg`,
+    message: messages[Math.floor(Math.random() * messages.length)],
+    name: names[Math.floor(Math.random() * names.length)]
+  };
+};
+
+const createPhoto = function(picture){
+  return {
+    id: picture.id,
+    url: picture.url,
+    description: picture.description,
+    likes: picture.likes,
+    comments: picture.comments
+  };
+};
+
+const createArray = (pictures) =>{
+  const photos = [];
+  let i = 0;
+  pictures.forEach((picture) => {
+    photos[i] = (createPhoto(picture));
+    i+=1;
+  });
+  drawPictures(photos);
+};
+
+const createError = (errorText) => {
+  const errorMessage = document.createElement('div');
+  errorMessage.style.backgroundColor = '#8C191B';
+  errorMessage.style.left = 0;
+  errorMessage.style.right = 0;
+  errorMessage.style.top = 0;
+  errorMessage.style.fontSize = `${30}px`;
+  errorMessage.style.textAlign = 'center';
+  errorMessage.style.position = 'absolute';
+  errorMessage.style.padding = `${20}px`;
+  errorMessage.textContent = errorText;
+  document.body.append(errorMessage);
+};
+
+
+
+export {createComment};
+export {createError};
+export {createArray};
+
+
