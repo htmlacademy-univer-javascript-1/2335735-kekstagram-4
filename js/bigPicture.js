50c41e6290a7d524967e6affe5d338beac229e67
Nordwi <amosh.work@yandex.ru>
2023-12-22 22:22:55 +0500

    function fixed and final refactoring

---
+++ js/bigPicture.js
@@ -0,0 +1,42 @@
+const bigPicturePreview = document.querySelector('.big-picture__preview');
+const commentsSection = bigPicturePreview.querySelector('.social__comments');
+const commentTemplate = commentsSection.querySelector('.social__comment');
+const loadMoreComments = bigPicturePreview.querySelector('.comments-loader');
+var activeData;
+
+const creatComment = (data) =>{
+  const comment =
+  (commentTemplate.cloneNode(true));
+  comment.querySelector('.social__picture').setAttribute('src', data.avatar);
+  comment.querySelector('.social__picture').setAttribute('alt', data.name);
+  comment.querySelector('.social__text').textContent = data.message;
+
+  return comment;
+};
+
+const onMoreButtonClick = () => {
+  const newComments = activeData.comments.splice(0, 5).map(creatComment);
+  const shown = activeData.commentsTotal - activeData.comments.length;
+
+  bigPicturePreview.querySelector('.comments-shown').textContent = String(shown);
+  commentsSection.append(...newComments);
+  loadMoreComments.classList.toggle('hidden', shown === activeData.commentsTotal);
+};
+
+
+const previewLoad = (data) => {
+  activeData = {
+    ...structuredClone(data),
+    commentsTotal: data.comments.length
+  };
+
+  bigPicturePreview.querySelector('.big-picture__img img').setAttribute('src', activeData.url);
+  bigPicturePreview.querySelector('.social__caption').textContent = activeData.description;
+  bigPicturePreview.querySelector('.likes-count').textContent = String(activeData.likes);
+  bigPicturePreview.querySelector('.comments-count').textContent = String(activeData.commentsTotal);
+  commentsSection.replaceChildren();
+  loadMoreComments.addEventListener('click', onMoreButtonClick);
+  loadMoreComments.click();
+};
+
+export default previewLoad;
---
+++ js/util.js
@@ -0,0 +1,25 @@
+export const call = async (url, options) => {
+  const response = await fetch(url, options);
+
+  if (!response.ok) {
+    throw new Error(`${response.status}. ${response.statusText}`);
+  }
+
+  return response.json();
+};
+
+export const debounce = (callback, delay = 500) => {
+  var statusTimeout;
+  var latestCallTime;
+
+  return (...rest) => {
+    const passedTime = Date.now() - latestCallTime;
+    const callDelay = Math.max(delay - passedTime, 0);
+    clearTimeout(statusTimeout);
+
+    statusTimeout = setTimeout(() => {
+      callback(...rest);
+      latestCallTime = Date.now();
+    }, callDelay);
+  };
+};
